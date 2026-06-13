4cb343a12dcee2fd8fc94f6aa6e0af7c02cdd075
surasak <surasak230347@gmail.com>
2026-06-13 14:04:24 +0700

    step 10

---
+++ lib/model/person.dart
@@ -0,0 +1,14 @@
+class Person {
+  String name;
+  int age;
+  String job;
+
+  Person({required this.name, required this.age, required this.job});
+}
+List<Person> people = [
+  Person(name: "สมชาย", age: 30, job: "นักเรียน"),
+  Person(name: "สมศรี", age: 25, job: "ครู"),
+  Person(name: "สมปอง", age: 40, job: "พ่อค้า"),
+  Person(name: "สมหมาย", age: 35, job: "ช่าง"),
+]
+;
--- lib/screen/item.dart
+++ lib/screen/item.dart
@@ -1,32 +1,44 @@
-import "package:flutter/material.dart";
-class Item extends StatefulWidget {
-  const Item({super.key});
+  import 'package:flutter/material.dart';
+  import 'package:learn_flutter_68_2/model/person.dart';
 
-  @override
-  State<Item> createState() => _ItemState();
-}
-class _ItemState extends State<Item> {
- List data = ["สมชาย", "สมศรี", "สมปอง", "สมหมาย"];
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: data.length,
-       itemBuilder: (context, index) {
-        return Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.pinkAccent),
-            borderRadius: BorderRadius.circular(10),
+  class Item extends StatefulWidget {
+    const Item({super.key});
 
-          ),
-          margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          padding: EdgeInsets.all(40),
-          child: Text(
-            data[index],
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-        );
-       },
-    );
+    @override
+    State<Item> createState() => _ItemState();
   }
-}
 
+  class _ItemState extends State<Item> {
+    List data = ["สมหมาย", "สมศรี", "สมปอง", "สมจิตร", "สมพร"];
+
+    @override
+    Widget build(BuildContext context) {
+      return ListView.builder(
+        itemCount: people.length,
+        itemBuilder: (content, index) {
+          return Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.pink),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            padding: EdgeInsets.all(40),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("ชื่อ: ${people[index].name}", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    Text("อายุ: ${people[index].age}", style: TextStyle(fontSize: 16)),
+                    Text("อาชีพ: ${people[index].job}", style: TextStyle(fontSize: 16)),
+                  ],
+                ),
+                Icon(Icons.person, size: 50, color: Colors.pinkAccent),
+              ],
+            ),
+          );
+        },
+      );
+    }
+  }
