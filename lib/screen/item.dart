ed6dd8be99d6bb0c215db1d3bd5990222ec49612
surasak <surasak230347@gmail.com>
2026-06-13 13:00:31 +0700

    step 8

---
+++ lib/screen/item.dart
@@ -0,0 +1,50 @@
+import "package:flutter/material.dart";
+class Item extends StatefulWidget {
+  const Item({super.key});
+
+  @override
+  State<Item> createState() => _ItemState();
+}
+class _ItemState extends State<Item> {
+  int quantity = 10;
+
+  void add() {
+    setState(() {
+      quantity++;
+    });
+  }
+  void remove() {
+    setState(() {
+      quantity = quantity > 0 ? quantity - 1 : 0;
+    });
+  }
+  @override
+  widget build(BuildContext context) {
+    return Center(
+      child: Column(
+       mainAxisAlignment: MainAxisAlignment.center
+       children:[
+        Text(
+          "จำนวนสินค้า: $quantity", 
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+        SizedBox(height: 20),
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: add, child: Text("เพิ่มสินค้า")),
+              SizedBox(width: 20),
+              ElevatedButton(onPressed: remove, child: Text("ลดสินค้า")),
+
+            ],
+
+          ),    
+
+        ],
+
+
+      ),
+
+    );
+
+  } 
+}
