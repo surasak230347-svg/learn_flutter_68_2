550d34a9fa5dfcde1c10ab4662155760d74bd6a4
surasak <surasak230347@gmail.com>
2026-06-20 13:46:15 +0700

    step 14

--- lib/screen/addform.dart
+++ lib/screen/addform.dart
@@ -11,6 +11,10 @@ class AddForm extends StatefulWidget {
 }
 
 class _AddFormState extends State<AddForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = '';
+  int _age = 0;
+  Job _job = Job.developer;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,50 +26,128 @@ class _AddFormState extends State<AddForm> {
           centerTitle: true,
         ),
         body:Padding(padding: EdgeInsetsGeometry.all(20.0),
+        // child: Column(
+        //   children: [
+        //     TextFormField(
+        //       decoration: InputDecoration(
+        //         labelText: "Name",
+        //       ),
+        //       onChanged: (value) {
+        //         _name = value;
+        //       },
+        //     ),
+        //     TextFormField(
+        //       decoration: InputDecoration(
+        //         labelText: "Age", keyboardType: TextInputType.number,
+        //       ),
+        //       onChanged: (value) {
+        //         _age = int.tryParse(value) ?? 0;
+        //       },
+        //     ),
+        //     DropdownButtonFormField(
+        //       decoration: InputDecoration(
+        //         labelText: "  Job",
+        //       ),
+        //       items: Job.values.map((key) {
+        //          final job = key;
+        //         return DropdownMenuItem(
+        //           value: key,
+        //           child: Text(key.title),
+        //         );
+        //       }).toList(),
+        //       onChanged: (value) {
+        //         _job = value ?? Job.developer;
+        //         print("Selected job: ${value?.title}");
+          
+        //       },   
+        //     ),
+        //     SizedBox(height: 20),
+        //     FilledButton(
+        //       style : FilledButton.styleFrom(
+        //         backgroundColor: Colors.pinkAccent,
+        //         foregroundColor: Colors.white,
+        //         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        //       ),
+        //       onPressed: () {
+        //         if (_formKey.currentState!.validate()) {
+        //           print("เพิ่มข้อมูลแล้ว");
+        //         }
+        //       },
+        //       child: Text("เพิ่มข้อมูล", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        //     ),
+        //   ]
+        // ,)
+        child: Form(
+        key: _formKey,
         child: Column(
           children: [
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: "Name",
-              ),
+
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: "Name",
+
             ),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: "Age", keyboardType: TextInputType.number,
-              ),
-            ),
-            DropdownButtonFormField(
-              decoration: InputDecoration(
-                labelText: "  Job",
-              ),
-              items: Job.values.map((key) {
-                 final job = key;
-                return DropdownMenuItem(
-                  value: key,
-                  child: Text(key.title),
-                );
-              }).toList(),
-              onChanged: (value) {
-                print("Selected job: ${value?.title}");
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'กรุณากรอกชื่อ';
+              }
+              if (int.tryParse(value) != null) {
+                return 'กรุณากรอกชื่อเป็นตัวอักษร';
+              }
+              return null;
+            },
+
+            onSaved: (value) {
+              _age = int.tryParse(value ?? '') ?? 20;
+            },
+          ),  
           
-              },   
-            ),
+          DropdownButtonFormField<Job>(
+            decoration: InputDecoration(labelText: "Job"),
+            items: Job.values.map((job) {
+              return DropdownMenuItem<Job>(
+                value: job,
+                child: Text(job.title),
+              );
+            }).toList(),
+            onChanged: (value) {
+              setState(() {
+                _job = value ?? Job.developer;
+      
+              });
+            },  
+          ),
+
+
             SizedBox(height: 20),
             FilledButton(
-              style : FilledButton.styleFrom(
-                backgroundColor: Colors.pinkAccent,
-                foregroundColor: Colors.white,
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              ),
+
               onPressed: () {
-                print("เพิ่มข้อมูลแล้ว");
+                if (_formKey.currentState!.validate()) {
+                  _formKey.currentState!.save();
+                  print("Name: $_name, Age: $_age, Job: ${_job.title}");
+                }
               },
-              child: Text("เพิ่มข้อมูล", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-            ),
-          ]
-        ,)
-        ,)
+                   setState(() {
+                     people.add(Person(name: _name, age: _age, job: _job)); 
+
+
+
+                    }),
+                    
+
+            },
+            
+          ),
+        ]
+
+        ),
+
+
       ),
-    );
-  }
-}
+
+
+    );  }
+} 
+
+
